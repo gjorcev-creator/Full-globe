d89ef7d4c0de57d966f0189d765bcb60c6a98fba
gjorcev-creator <gjorcev@gmail.com>
2026-03-28 22:28:32 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobeView from "./GlobeView";
+import "../styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobeView />
+    <div className="app">
+      <GlobeView />
+    </div>
   </React.StrictMode>
 );
